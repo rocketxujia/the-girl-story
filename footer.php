001dbde420b0e3a015cff0fa61c5dd4c363c9a13
Joe Xu <rocketxj@gmail.com>
2012-09-27 09:38:48 +0800

    添加google analyse

--- footer.php
+++ footer.php
@@ -46,23 +46,21 @@
      * tag of your theme, or you will break many plugins, which
      * generally use this hook to reference JavaScript files.
      */
-
 wp_footer();
 ?>
 
-<!-- Asynchronous google analytics; this is the official snippet.
-     Replace UA-XXXXXX-XX with your site's ID and uncomment to enable.
-<script>
+<script type="text/javascript">
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-XXXXXX-XX']);
-  _gaq.push(['_trackPageview']);
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-35130132-1']);
+    _gaq.push(['_setDomainName', 'xjsxj.com']);
+    _gaq.push(['_trackPageview']);
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+    (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
 
 </script>
 -->
